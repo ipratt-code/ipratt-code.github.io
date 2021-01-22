4c773f8660b42979ba3e2a0a70cbc9a8e2076276
Ian Pratt <thingcoder1@gmail.com>
2021-01-22 00:28:07 +0000

    POST request to db works!

--- static/js/login.js
+++ static/js/login.js
@@ -1,13 +1,19 @@
 // stops form from reloading page (thx stackoverflow!!!)
 var form = document.getElementById("loginform");
-function handleForm(event) { event.preventDefault(); } 
+function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', handleForm);
 
 // applying js func to login button
-document.getElementById("loginbtn").onclick = login;
-function login() {
-	var formdata = $('form').serializeJSON()
-	console.log(formdata);
-	document.getElementById("usernamereturn").textContent = "Your username: " + formdata.username
-	document.getElementById("passwordreturn").textContent = "Your password: " + formdata.password
+document.getElementById("loginbtn").onclick = getLoginResponse;
+function updatePage(data) {
+	document.getElementById("datareturn").textContent = "Returned data: " + JSON.stringify(data);
+}
+
+function getLoginResponse() {
+	$.post("https://ipratt-codedb.thingcoder1.repl.co/login",
+	$('form').serializeJSON(),
+	function(data){
+		data = JSON.parse(data);
+		updatePage(data);
+    });
 }
