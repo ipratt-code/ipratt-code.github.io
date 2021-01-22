8c55db873b2a18f036716762296e5d1a891d2a5a
Ian Pratt <thingcoder1@gmail.com>
2021-01-22 17:11:13 +0000

    added login/register for website, idk why

--- static/js/register.js
+++ static/js/register.js
@@ -1,28 +1,26 @@
 // stops form from reloading page (thx stackoverflow!!!)
-var form = document.getElementById("loginform");
+var form = document.getElementById("registerform");
 function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', handleForm);
 
 // applying js func to login button
-document.getElementById("loginbtn").onclick = getLoginResponse;
+document.getElementById("registerbtn").onclick = getRegisterResponse;
+
 function updatePage(data) {
 	document.getElementById("datareturn").textContent = "Returned data: " + JSON.stringify(data);
 }
 
-function getLoginResponse() {
+function getRegisterResponse() {
 	var formJson = $('form').serializeJSON()
-	formJson.type = "login"
-	$.post("https://ipratt-codedb.thingcoder1.repl.co/login",
+	formJson.type = "register"
+	$.post("https://ipratt-codedb.thingcoder1.repl.co/register",
 	formJson,
 	function(data){
 		data = JSON.parse(data);
 		if(data.error != undefined){
-			if(data.error == "username does not exist"){
-				window.alert("username does not exist");
+			if(data.error == "username is already taken"){
+				window.alert("username is already taken");
 				location.href = 'register';
-			} else if(data.error == "incorrect password"){
-				window.alert("incorrect password");
-				location.href = 'login';
 			}
 
 		}
