adb46677002c5c0922cf8b8e1324454015722fbe
Ian Pratt <thingcoder1@gmail.com>
2021-01-25 01:19:11 +0000

    target blnk

--- static/js/nodtalk.js
+++ static/js/nodtalk.js
@@ -11,7 +11,7 @@ function toggleAutoScroll() {
 function urlify(text) {
   var urlRegex = /(https?:\/\/[^\s]+)/g;
   return text.replace(urlRegex, function(url) {
-    return '<a href="' + encodeURI(url) + '">' + encodeURI(url) + '</a>';
+    return '<a href="' + encodeURI(url) + '" target="_blank">' + encodeURI(url) + '</a>';
   })
   // or alternatively
   // return text.replace(urlRegex, '<a href="$1">$1</a>')
